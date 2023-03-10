408d483f4c8458f097c25e96400081e873da1a77
GHKirill <103307710+GHKirill@users.noreply.github.com>
2023-03-10 14:10:21 +0000

    Deploying to gh-pages from @ GHKirill/goit-js-YT3@dd558918e479935d756ff48eb9c90d5aab85c58f 🚀

---
+++ index.97aba798.js
@@ -0,0 +1,2 @@
+fetch("https://googleapis.com/youtube/v3/search?key=AIzaSyCOd7Y3jAZspvMKYZ_dqAmR1Z0wReTUeh0?publishedAfter='1970-01-01T00:00:00Z'?q='pirate'?maxResults=1").then((function(e){return console.log(e)}));
+//# sourceMappingURL=index.97aba798.js.map
